66faafd9c5821830f93b95d0f846f330cea7a4c8
KonstantinChuper <konstantyn.chuper@gmail.com>
2024-06-24 09:18:18 +0200

    main logic

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -1,11 +1,44 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import FormItem from "./FormItem";
 
 export default function ToDoList() {
   const [listItems, setListItems] = useState([]);
+
   function addTask(text) {
-    setListItems(listItems.concat({ text, isChecked: false }));
+    const newTask = { id: Date.now(), text, isChecked: false };
+    const updatedList = [...listItems, newTask];
+    setListItems(updatedList);
+    localStorage.setItem("tasks", JSON.stringify(updatedList));
   }
+
+  function deleteTask(id) {
+    const updatedList = listItems.filter((item) => {
+      return item.id !== id;
+    });
+    setListItems(updatedList);
+    localStorage.setItem("tasks", JSON.stringify(updatedList));
+  }
+
+  function toggleCheckbox(id) {
+    const updatedList = listItems.map((item) => {
+      if (item.id === id) {
+        return { ...item, isChecked: !item.isChecked };
+      }
+      return item;
+    });
+    setListItems(updatedList);
+    localStorage.setItem("tasks", JSON.stringify(updatedList));
+  }
+
+  console.log(listItems);
+
+  useEffect(() => {
+    const storedTasks = localStorage.getItem("tasks");
+    if (storedTasks) {
+      setListItems(JSON.parse(storedTasks));
+    }
+  }, []);
+
   return (
     <div className="container">
       <FormItem addTask={addTask} />
@@ -13,9 +46,9 @@ export default function ToDoList() {
         {listItems.map((item, index) => {
           return (
             <li key={index}>
-              <input type="checkbox" name="" id="" />
-              <span>{item.text}</span>
-              <button>Delete</button>
+              <input type="checkbox" checked={item.isChecked} onChange={() => {toggleCheckbox(item.id)}} />
+              <span>{item.text}</span> 
+              <button onClick={() => deleteTask(item.id)}>Delete</button>
             </li>
           );
         })}
